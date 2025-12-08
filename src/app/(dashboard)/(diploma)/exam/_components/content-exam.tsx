cd9d698b81c90394f74e82bdae361987db07ac5e
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2025-12-08 12:21:25 +0200

    feat:add loading for exam

--- src/app/(dashboard)/(diploma)/exam/_components/content-exam.tsx
+++ src/app/(dashboard)/(diploma)/exam/_components/content-exam.tsx
@@ -1,25 +1,20 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { GetExamContent } from '../_hooks/get-exam-content';
 
-export default function ContentExam({
-  title,
-  duration,
-  numberOfQuestions,
-}: {
-  title: string;
-  duration: string;
-  numberOfQuestions: number;
-}) {
+export default async function ContentExam() {
+  const { data, error } = await GetExamContent();
   return (
-    <Link href={'/'}>
-    <section className=" flex flex-col md:flex md:flex-row  h-[5rem] md:items-center md:justify-between bg-blue-50 px-4 hover:bg-blue-100" title={title}>
+    <>
+    {data?.exams.map((item)=>(<Link key={item._id} href={'/'}>
+    <section className=" flex flex-col md:flex md:flex-row  h-[5rem] md:items-center md:justify-between bg-blue-50 px-4 hover:bg-blue-100" title={item.title}>
       {/* Left Part */}
       <div className="flex flex-col gap-1">
-        <h1 className=" text-base sm:text-lg md:text-xl font-semibold text-blue-600">{title}</h1>
+        <h1 className=" text-base sm:text-lg md:text-xl font-semibold text-blue-600">{item.title}</h1>
 
         <p className="text-xs sm:text-sm font-normal text-gray-500">
-          {numberOfQuestions} Questions{' '}
+          {item.numberOfQuestions} Questions{' '}
         </p>
       </div>
       {/* Right Part */}
@@ -30,10 +25,12 @@ export default function ContentExam({
           height={24}
           alt="timer"
         />
-        <p className='text-gray-800 font-normal text-xs sm:text-sm' >Duration: {duration} minutes </p>
+        <p className='text-gray-800 font-normal text-xs sm:text-sm' >Duration: {item.duration} minutes </p>
       </div>
 
     </section>
-    </Link>
+    </Link>))}
+    
+    </>
   );
 }
