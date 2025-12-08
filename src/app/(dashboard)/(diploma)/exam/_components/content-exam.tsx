5c76fea8e845dc6595e8e29abd5aa798c0e1068c
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2025-12-08 13:08:42 +0200

    feat:create not-found page

--- src/app/(dashboard)/(diploma)/exam/_components/content-exam.tsx
+++ src/app/(dashboard)/(diploma)/exam/_components/content-exam.tsx
@@ -7,30 +7,43 @@ export default async function ContentExam() {
   const { data, error } = await GetExamContent();
   return (
     <>
-    {data?.exams.map((item)=>(<Link key={item._id} href={'/'}>
-    <section className=" flex flex-col md:flex md:flex-row  h-[5rem] md:items-center md:justify-between bg-blue-50 px-4 hover:bg-blue-100" title={item.title}>
-      {/* Left Part */}
-      <div className="flex flex-col gap-1">
-        <h1 className=" text-base sm:text-lg md:text-xl font-semibold text-blue-600">{item.title}</h1>
+      {data?.exams.map((item) => (
+        <Link
+          key={item._id}
+          href={{
+            pathname: `/exam/${item._id}/question`,
+            query: { title: item.title },
+          }}
+        >
+          <section
+            className="flex h-[5rem] flex-col bg-blue-50 px-4 hover:bg-blue-100 md:flex md:flex-row md:items-center md:justify-between"
+            title={item.title}
+          >
+            {/* Left Part */}
+            <div className="flex flex-col gap-1">
+              <h1 className="text-base font-semibold text-blue-600 sm:text-lg md:text-xl">
+                {item.title}
+              </h1>
 
-        <p className="text-xs sm:text-sm font-normal text-gray-500">
-          {item.numberOfQuestions} Questions{' '}
-        </p>
-      </div>
-      {/* Right Part */}
-      <div className="flex gap-1 items-center">
-        <Image
-          src={'/assets/icons/timer.svg'}
-          width={24}
-          height={24}
-          alt="timer"
-        />
-        <p className='text-gray-800 font-normal text-xs sm:text-sm' >Duration: {item.duration} minutes </p>
-      </div>
-
-    </section>
-    </Link>))}
-    
+              <p className="text-xs font-normal text-gray-500 sm:text-sm">
+                {item.numberOfQuestions} Questions{' '}
+              </p>
+            </div>
+            {/* Right Part */}
+            <div className="flex items-center gap-1">
+              <Image
+                src={'/assets/icons/timer.svg'}
+                width={24}
+                height={24}
+                alt="timer"
+              />
+              <p className="text-xs font-normal text-gray-800 sm:text-sm">
+                Duration: {item.duration} minutes{' '}
+              </p>
+            </div>
+          </section>
+        </Link>
+      ))}
     </>
   );
 }
