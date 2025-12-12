791f56febd5eb26a6ee79c2015885dd69060ee42
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2025-12-12 22:01:56 +0200

    refactor:edit loading

--- src/app/(auth)/loading.tsx
+++ src/app/(auth)/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/components/loaders/Spinner";
 
 export default function Loading() {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex h-screen lg:h-full items-center justify-center">
       <Spinner color="text-blue-600" />
     </div>
   );
