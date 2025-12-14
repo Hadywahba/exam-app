de65331973570f1c548e4e07a5368230ed68d4ab
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2025-12-14 14:25:29 +0200

    refactor:update email schema

--- src/lib/schemas/login.schema.ts
+++ src/lib/schemas/login.schema.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 
 export const loginschema = z.object({
-  email: z.string().nonempty(' your email is required'),
+  email: z.string().nonempty(' your email is required').email('Invalid email'),
 
   password: z.string().nonempty(' your password is required'),
 });
