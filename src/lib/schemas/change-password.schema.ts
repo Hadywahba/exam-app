ccbae6915e7328b9fa926b2b1f367d97928c59e1
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2025-12-14 13:40:31 +0200

    refactor:update password schema

--- src/lib/schemas/change-password.schema.ts
+++ src/lib/schemas/change-password.schema.ts
@@ -7,37 +7,18 @@ export const changepasswordschema = registerschema
     oldPassword: z
       .string()
       .nonempty(' your  Password is required')
-      .superRefine((value, ctx) => {
-        if (value.length < 8) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: 'Password must be at least 8 characters',
-          });
-        }
-        if (!/[A-Z]/.test(value)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: 'Password must contain at least one uppercase letter',
-          });
-        }
-        if (!/[a-z]/.test(value)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: 'Password must contain one lowercase letter',
-          });
-        }
-        if (!/[0-9]/.test(value)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: 'Password must contain one number',
-          });
-        }
-        if (!/[@$!%*?&]/.test(value)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: 'Password must contain one special character',
-          });
-        }
+      .min(8, { message: 'Password must be at least 8 characters long.' })
+      .refine((password) => /[A-Z]/.test(password), {
+        message: 'Password must contain at least one uppercase letter.',
+      })
+      .refine((password) => /[a-z]/.test(password), {
+        message: 'Password must contain at least one lowercase letter.',
+      })
+      .refine((password) => /[0-9]/.test(password), {
+        message: 'Password must contain at least one number.',
+      })
+      .refine((password) => /[!@#$%^&*]/.test(password), {
+        message: 'Password must contain at least one special character.',
       }),
   })
 
